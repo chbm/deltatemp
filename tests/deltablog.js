d3d9da9f8bccc3e639bdb78285f07f984a0a9ceb
carlos morgado <cchhbbmm@gmail.com>
2008-12-15 00:47:39 +0000

    blog page demo

---
+++ tests/deltablog.js
@@ -0,0 +1,32 @@
+/**
+ * @author chbm
+ */
+var contents = {
+	now: function(elem){
+        return Date().toString();
+    },
+	loggedin: false,
+	notloggedin: true, //ok this sucks
+	username: ''	
+}
+var dt;
+
+function updatelogin() {
+	contents.username = $('#usernamebox').val();
+	contents.notloggedin = false;
+	dt.updateValues('notloggedin');
+	contents.loggedin = true;
+	dt.updateValues('username'); // shouldn't be needed!!
+	dt.updateValues('loggedin');	
+}
+
+$(document).ready(function() {
+    dt = new DeltaTemp(contents);
+    dt.processDocument();		
+		
+	$.getJSON('posts.js', function(data) {
+		contents.posts = data;
+		dt.updateValues('posts');	
+	} );
+} );
+
---
+++ tests/posts.js
@@ -0,0 +1,8 @@
+/**
+ * @author chbm
+ */
+[
+	{title: 'The top post', body: 'This is the post body and a rather long string. Blah blah blah blah blah blah blah blah blah blah blah blahb albhahsdhasdfasfjkasdhksadjfasdkjfhkjlasdkjasdhjk asjldhfalksjdhflkjasdhflkasjdhfk', about: 'posted by santa claus on 30/2/2010'},
+	{title: 'The second post', body: 'This is the post body and a rather long string. Blah blah blah blah blah blah blah blah blah blah blah blahb albhahsdhasdfasfjkasdhksadjfasdkjfhkjlasdkjasdhjk asjldhfalksjdhflkjasdhflkasjdhfk', about: 'posted by santa claus on 30/2/2010'},
+	{title: 'Bronzist', body: 'This is the post body and a rather long string. Blah blah blah blah blah blah blah blah blah blah blah blahb albhahsdhasdfasfjkasdhksadjfasdkjfhkjlasdkjasdhjk asjldhfalksjdhflkjasdhflkasjdhfk', about: 'posted by santa claus on 30/2/2010'}
+]
