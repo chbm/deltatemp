cc199f8608ef91ec68958830342d41019a3f07c6
carlos morgado <cchhbbmm@gmail.com>
2009-01-31 16:00:18 +0000

    API update

--- tests/deltablog.js
+++ tests/deltablog.js
@@ -15,24 +15,24 @@ var dt;
 function updateLogin() {
 	contents.username = 'logging in ' + $('#usernamebox').val() + ' ...';
 	contents.notloggedin = false;
-	dt.updateValues('notloggedin');
+	dt.updateTree('notloggedin');
 	contents.loggedin = true;
-	dt.updateValues('loggedin');	
+	dt.updateTree('loggedin');	
 	setTimeout(function() {
 		contents.username = 'hello, ' + $('#usernamebox').val();
-		dt.updateValues('username');
+		dt.updateTree('username');
 	}, 3000);	
 	return false;
 }
 
 function showMorePosts() {
 	contents.morepostsnotloaded = false;
-	dt.updateValues('morepostsnotloaded');
+	dt.updateTree('morepostsnotloaded');
 	
 	$.getJSON('moreposts.js', function(data) {
 		
 		jQuery.each(data, function() {contents.posts.push(this)});
-		dt.updateValues('posts');	
+		dt.updateTree('posts');	
 	} );
 }
 
@@ -42,7 +42,7 @@ $(document).ready(function() {
 		
 	$.getJSON('posts.js', function(data) {
 		contents.posts = data;
-		dt.updateValues('posts');	
+		dt.updateTree('posts');	
 	} );
 } );
 
